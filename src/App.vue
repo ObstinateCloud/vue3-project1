d4191f714e9fc65e82fffca5612bb61331dea675
zhangjianyun <zhangjianyun_kd@163.com>
2024-12-15 14:48:49 +0800

    引入ant-design依赖

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import LayoutIndex from './views/layout/LayoutIndex.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <LayoutIndex/>
+  </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  background-color: aqua;
+  height: 100vh;
+  width: 1800px;
+  display: flex;
+  overflow-x: hidden;
+  overflow-y: auto;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import * as Icons from "@ant-design/icons-vue";
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const icons: any = Icons
+for (const i in icons) {
+  app.component(i, icons[i])
+}
+app.use(Antd).mount('#app')
